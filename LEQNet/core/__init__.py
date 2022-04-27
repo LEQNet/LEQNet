9ebde095bd3ad58321690dd6c5adaf9ff93a7fd8
Jung Sunghun <65435447+seoungheong@users.noreply.github.com>
2022-04-27 17:12:00 +0900

    Add files via upload

---
+++ LEQNet/core/__init__.py
@@ -0,0 +1,9 @@
+
+name='core'
+
+from .LEQNet_utils import *
+from .trainer import trainer
+from .tester import tester
+from .predictor import predictor
+from .mseed_predictor import mseed_predictor
+
